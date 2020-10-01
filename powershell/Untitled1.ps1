e89eae2a61008cac12dc0cb4bca5f2d5c09f2330
JakePeralta7 <eladgamer7@gmail.com>
2020-10-01 22:05:02 +0300

    Create Untitled1.ps1

---
+++ powershell/Untitled1.ps1
@@ -0,0 +1,12 @@
+﻿$processes = Get-Process -Name *win*
+
+foreach ($process in $processes){
+    $name = $process.Name
+    $cpu = $process.CPU
+    if ($cpu -gt 3){
+        write-host "The process '$name' is a cpu eater"
+    }
+    else{
+        write-host "The process '$name' is not a cpu eater"
+    }
+}
